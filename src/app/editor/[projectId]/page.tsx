e21dca5fd9a4b3f7e0e53328a554c8b5c835e850
Yash Rajbhar <yashraj78289@gmail.com>
2025-04-03 17:09:48 +0530

    Added fabricjs and js dom and created canvas editor

---
+++ src/app/editor/[projectId]/page.tsx
@@ -0,0 +1,10 @@
+import { Editor } from "@/features/editor/components/editor";
+const EditorProjectIdPage = () => {
+    return ( 
+        <div className="h-full">
+            <Editor />
+        </div>
+     );
+}
+ 
+export default EditorProjectIdPage;
